5c2276a0208bd0fe2fb878c26b6ed36c5cfc92a5
cellva01 <selva@swarajyamag.com>
2023-05-31 15:39:40 +0530

    installing chrome

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import os
 
 app = FastAPI()
 
-# try:
-#     os.system('sudo apt-get update')
-#     os.system('apt-get install -y libglib2.0 libnss3 libgconf-2-4 libfontconfig1')
-# except Exception as e:
-#     print("Error in installing chromium-browser: ", e)
+try:
+    os.system('sudo apt-get update')
+    os.system('apt-get install -y libglib2.0 libnss3 libgconf-2-4 libfontconfig1')
+except Exception as e:
+    print("Error in installing chromium-browser: ", e)
 
 
 #use apscheduler to run check_sites every 10 minutes
