79a0ef57b87cbf30f943abd7342213b210ab1df5
cellva01 <selva@swarajyamag.com>
2023-05-31 15:58:10 +0530

    path

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app = FastAPI()
 try:
     os.system('sudo apt-get update')
     os.system('apt-get install -y libglib2.0 libnss3 libgconf-2-4 libfontconfig1')
-    print("Packages installed Path: ", os.system('which chromium-browser'))
-    print("Packages installed Path: ", os.system('which google-chrome'))
+    print("Packages installed Paths: ", os.system('whereis chromium-browser'))
+    print("Packages installed Paths: ", os.system('whereis google-chrome'))
 
 except Exception as e:
     print("Error in installing chromium-browser: ", e)
